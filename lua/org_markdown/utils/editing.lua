3a2dea442e81f27a30fd406693ebcae7d50c491b
Konstantin Astafurov <konstantin.astafurov@gmail.com>
2025-07-06 19:03:45 -0400

    todo lists and config defaults

--- lua/org_markdown/utils/editing.lua
+++ lua/org_markdown/utils/editing.lua
@@ -23,16 +23,39 @@ function M.cycle_checkbox_in_line(line, states)
 	end
 
 	local next_state = states[(index % #states) + 1]
-	return line:gsub(pattern, "- [" .. next_state .. "]", 1)
+	return { (line:gsub(pattern, "- [" .. next_state .. "]", 1)) }
 end
 
-function M.edit_line_at_cursor(modifier_fn)
+function M.continue_todo(line)
+	local pattern = "%- %[.%](.*)"
+	local current = line:match(pattern)
+
+	-- early return if no checkbox found
+	if not current then
+		return nil
+	end
+
+    if current == "" or current == " " then
+        return { "" }
+    end
+
+    return { line, "- [ ] " }
+end
+
+function M.edit_line_at_cursor(modifier_fn, update_cursor)
 	local row = vim.fn.line(".") - 1
 	local line = vim.api.nvim_buf_get_lines(0, row, row + 1, false)[1]
-	local new_line = modifier_fn(line)
-	if new_line and new_line ~= line then
-		vim.api.nvim_buf_set_lines(0, row, row + 1, false, { new_line })
-	end
+	local new_lines = modifier_fn(line)
+
+	if new_lines then
+		vim.api.nvim_buf_set_lines(0, row, row + 1, false, new_lines)
+        if update_cursor then
+            vim.api.nvim_win_set_cursor(0, { row + #new_lines , #new_lines[#new_lines] })
+        end
+        return true
+    else
+    return false
+end
 end
 
 function M.setup_editing_keybinds(bufnr)
@@ -44,6 +67,20 @@ function M.setup_editing_keybinds(bufnr)
 		desc = "Cycle checkbox state",
 		buffer = bufnr,
 	})
+
+	vim.keymap.set("i", "<CR>", function()
+		local edited = M.edit_line_at_cursor(function(line)
+			return M.continue_todo(line)
+		end, true)
+
+        if not edited then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", false)
+        end
+	end, {
+		desc = "extend todo list entries",
+		buffer = bufnr,
+	})
+
 end
 
 return M
